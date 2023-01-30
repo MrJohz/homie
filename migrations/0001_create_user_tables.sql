a7f75888dbfc8e909f4bb07a1b12abc8cafeaf03
Jonathan Frere <jonathan.frere@esveo.com>
2023-01-30 08:58:54 +0100

    Improve selection of last_completed date

--- migrations/0001_create_user_tables.sql
+++ migrations/0001_create_user_tables.sql
@@ -1,5 +1,8 @@
 CREATE TABLE
-    users (id text primary key, username text, hash text);
+    users (id integer primary key, username text, hash text);
 
 CREATE TABLE
-    tokens (token text UNIQUE, id text REFERENCES users (id));
+    tokens (
+        token text UNIQUE,
+        id integer REFERENCES users (id)
+    );
--- migrations/0002_create_task_tables.sql
+++ migrations/0002_create_task_tables.sql
@@ -1,22 +1,26 @@
 CREATE TABLE
   tasks (
-    id text primary key,
+    id integer primary key,
     task_name text NOT NULL,
     kind text NOT NULL,
-    duration integer NOT NULL,
-    start_assignee text NOT NULL,
-    first_done text NOT NULL
+    duration integer NOT NULL
   );
 
 CREATE TABLE
   task_participant_link (
-    task_id text NOT NULL REFERENCES tasks (id),
-    user_id text NOT NULL REFERENCES users (id)
+    task_id integer NOT NULL REFERENCES tasks (id),
+    user_id integer NOT NULL REFERENCES users (id),
+    UNIQUE (task_id, user_id)
   );
 
 CREATE TABLE
   completions (
-    task_id text NOT NULL REFERENCES tasks (id),
-    completed_by text NOT NULL REFERENCES users (id),
-    completed_on text NOT NULL
+    task_id integer NOT NULL REFERENCES tasks (id),
+    completed_by integer NOT NULL REFERENCES users (id),
+    completed_on text NOT NULL,
+    initial integer NOT NULL DEFAULT FALSE
   );
+
+CREATE INDEX task_participant_link_task_id ON task_participant_link (task_id);
+
+CREATE INDEX completions_completed_on_task_id ON completions (completed_on, task_id);
