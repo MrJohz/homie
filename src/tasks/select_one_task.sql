5ef28554393fa9a911e9d4244888b15aad4b4347
Jonathan Frere <jonathan.frere@gmail.com>
2023-01-30 16:06:03 +0100

    Improve SQL select queries

--- src/tasks/select_one_task.sql
+++ src/tasks/select_one_task.sql
@@ -2,24 +2,48 @@ SELECT
   tasks.task_name as name,
   tasks.kind as kind,
   tasks.duration as duration,
-  u_participants.username as participant,
-  tasks.first_done as first_done,
-  IFNULL (completions.completed_on, tasks.first_done) as done_at,
-  IFNULL (u_completed.username, u_assignee.username) as done_by
+  json_group_array (u_participants.username) as participants,
+  CASE tasks.kind
+    WHEN "INTERVAL" THEN last_completion.completed_on
+    WHEN "SCHEDULE" THEN date (
+      first_completion.completed_on,
+      '+' || (tasks.duration * coalesce(completion_count, 0)) || ' days'
+    )
+    ELSE NULL
+  END as last_completed,
+  u_completed.username as last_completed_by
 FROM
   tasks
   INNER JOIN task_participant_link ON tasks.id = task_participant_link.task_id
   INNER JOIN users u_participants ON u_participants.id = task_participant_link.user_id
-  INNER JOIN users u_assignee ON u_assignee.id = tasks.start_assignee
-  LEFT JOIN completions ON tasks.id = completions.task_id
-  AND completions.rowid = (
+  INNER JOIN completions last_completion ON tasks.id = last_completion.task_id
+  AND last_completion.completed_on = (
     Select
-      Max(rowid)
+      max(completed_on)
     from
       completions as c2
     where
       c2.task_id = tasks.id
   )
-  LEFT JOIN users u_completed ON u_completed.id = completions.completed_by
+  INNER JOIN users u_completed ON u_completed.id = last_completion.completed_by
+  INNER JOIN completions first_completion ON tasks.id = first_completion.task_id
+  AND first_completion.completed_on = (
+    Select
+      max(completed_on)
+    from
+      completions as c3
+    where
+      c3.task_id = tasks.id
+      AND c3.initial = TRUE
+  )
+  LEFT JOIN (
+    select
+      task_id,
+      count(*) as completion_count
+    FROM
+      completions _ccount
+    WHERE
+      _ccount.initial = FALSE
+  ) c4 ON c4.task_id = tasks.id
 WHERE
   tasks.id = ?
