334b8b3ce449c77da3bb15be364780a5046ae84d
Jonathan Frere <jonathan.frere@esveo.com>
2023-02-17 10:16:38 +0100

    Fix nested transactions

--- src/tasks/insert_new_task.sql
+++ src/tasks/insert_new_task.sql
@@ -1,16 +1,7 @@
 -- SPDX-FileCopyrightText: 2023 Jonathan Frere
 --
 -- SPDX-License-Identifier: MPL-2.0
-BEGIN TRANSACTION;
-
 INSERT INTO
   tasks (kind, duration)
 VALUES
-  (?, ?) RETURNING tasks.id;
-
-INSERT INTO
-  task_translations (task_id, task_name)
-VALUES
-  (last_insert_rowid (), ?);
-
-COMMIT TRANSACTION;
+  (?, ?) RETURNING tasks.id
